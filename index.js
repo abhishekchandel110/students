7b186f0dfc791ae27b09932e602cc4b32cc882a9
ayushsagar110 <sagarayush556@outlook.com>
2023-03-30 15:52:23 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require("express")
+
+const bodyparser = require("body-parser")
 const app = express()
+app.use(bodyparser.json())
+
+const db = []
 
 app.get("/", function(req, res){
-    res.send("hello")
+    res.send(db)
+    console.log(db)
 })
 
-app.listen(8000)
+app.put("/",function(req,res){
+    for(let i = 0; i<db.length; i++){
+        if(db[i].name===req.body.name){
+            db[i].age=req.body.age
+        }
+    }
+    res.send("Saved your update")
+})
+
+app.delete("/",function(req,res){
+    for(let i = 0; i<db.length; i++){
+        if(db[i].name===req.body.name){
+            db[i].age===req.body.age
+        }
+        
+    }
+    res.delete("deleted")
+})
+app.listen(8000 , function(){
+    console.log("start");
+})
