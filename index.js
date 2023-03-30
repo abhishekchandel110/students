96db7ddd68bb8be150aff93593f3003f6e64b092
abhishekchandel110 <chandelabhishek110@outlook.com>
2023-03-30 16:06:38 +0530

    updated

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/", function(req, res){
     console.log(db)
 })
 
+app.post("/", function(req, res){
+    db.push(req.body)
+    res.send(req.body.name + req.body.age)
+})
+
 app.put("/",function(req,res){
     for(let i = 0; i<db.length; i++){
         if(db[i].name===req.body.name){
@@ -23,11 +28,11 @@ app.put("/",function(req,res){
 app.delete("/",function(req,res){
     for(let i = 0; i<db.length; i++){
         if(db[i].name===req.body.name){
-            db[i].age===req.body.age
+            db.splice(i, 1)
         }
         
     }
-    res.delete("deleted")
+
 })
 app.listen(8000 , function(){
     console.log("start");
